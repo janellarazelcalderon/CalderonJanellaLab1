688748d842ec2d7db38677accb9fd9511368f9e3
imjanellarazel <janellarazelcalderon14@gmail.com>
2018-09-08 07:59:47 +0800

    Initial commit

---
+++ app/src/main/java/com/calderon/janella/CalderonJanellaLab1.java
@@ -0,0 +1,13 @@
+package com.calderon.janella;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CalderonJanellaLab1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calderon_janella_lab1);
+    }
+}
